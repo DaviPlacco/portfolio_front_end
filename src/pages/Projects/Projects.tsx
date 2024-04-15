cbff63188106915b22dca55ef61f7e3fee02c22c
Davi Placco <davibplacco@gmail.com>
2024-04-15 15:00:41 +0100

    Feat: Add route to the project folder

---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+export const Projects = () => {
+    return(
+        <>
+            <h2>
+                Projects
+            </h2>
+        </>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Page404 } from "./pages/Page404/Page404";
+import { Projects } from "./pages/Projects/Projects";
 
 
 function AppRoutes() {
@@ -9,6 +10,7 @@ function AppRoutes() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/Projects" element={<Projects />}></Route>
                 <Route path="*" element={<Page404 />}></Route>
             </Routes>
         </BrowserRouter>
