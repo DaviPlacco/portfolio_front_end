1448bf237b86efd3860c3fd403f272d2e91f421a
Davi Placco <davibplacco@gmail.com>
2024-04-18 16:17:21 +0100

    Feat: Add Hr or space in second section

---
+++ src/components/hr/Hr.tsx
@@ -0,0 +1,11 @@
+import Hrone from '../../assets/Hr-space/hrone.png'
+import HrTwo from '../../assets/Hr-space/hrtwo.png'
+
+export const HrSpace = () =>{
+    return(
+        <>
+            <img src={Hrone} alt="Hr-Space" className="select-none"/>
+            <img src={HrTwo} alt="Hr-Space" className="select-none"/>
+        </>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header/Header"
 import HeroImage from '../../assets/Herosection/heroimage.png'
+import { HrSpace } from "../../components/hr/Hr"
 
 
 export const Home = () => {
@@ -7,14 +8,14 @@ export const Home = () => {
     return(
         <>
            <Header/> 
-           <div className="w-full flex items-center justify-center">
+           <div className="w-full flex items-center justify-center pb-28">
             <div className="pt-28 h-screen w-[1280px] flex items-center justify-center">
                 <section className="w-1/2 sm:w-full flex flex-col items-start">
                     <h3 id="iam" className="text-[26px] font-light font-['Oxanium']">
                         HI I`M 
                     </h3>
-                    <h1 className="text-white text-[100px] font-light font-['Oxanium']">
-                    Davi B. Placco
+                    <h1 id="DaviPlacco" className="text-white text-[100px] font-light font-['Oxanium']">
+                        Davi B. Placco
                     </h1>
                     <h3 id="frontend" className="text-[26px] font-light font-['Oxanium']">
                         Front-end Developer
@@ -27,6 +28,9 @@ export const Home = () => {
                     <img src={HeroImage} alt="heroimage" draggable="false" className="h-[1100px] pointer-events-none select-none"/>
                 </section>
              </div>
+            </div>
+            <div>
+                <HrSpace/>
             </div>
         </>
     )
