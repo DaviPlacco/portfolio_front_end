1a161593fe7e9ab22c6ee25e83e411a0060d3421
Davi Placco <davibplacco@gmail.com>
2024-04-19 16:27:29 +0100

    Feat: Create the page 404, and add Route to go back to the main page (Home)

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom"
+
 export const Page404 = () => {
     return(
         <>
-            <h2>
-                Page 404
-            </h2>
+            <section className="w-full h-screen flex flex-col justify-center items-center gap-6">
+                <h2 className="text-white text-8xl font-['Oxanium']">
+                    Page 404
+                </h2>
+                <p className="text-white font-['inter']">
+                    oops.. Nothing to see here
+                </p>
+                <Link to={"/"} className="text-white underline decoration-1">Go back to the content</Link>
+            </section>
         </>
     )
 }
