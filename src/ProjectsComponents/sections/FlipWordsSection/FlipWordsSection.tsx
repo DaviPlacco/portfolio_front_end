c72b785d00041a03ab769a4fe1442705a5cdf5a8
Davi Placco <davibplacco@gmail.com>
2024-06-30 23:05:44 +0100

    Feat: add some links in Carousel

--- src/ProjectsComponents/sections/FlipWordsSection/FlipWordsSection.tsx
+++ src/ProjectsComponents/sections/FlipWordsSection/FlipWordsSection.tsx
@@ -6,7 +6,7 @@ export const FlipWordsSection = () => {
     return (
         <>
             <div id="FlipWordsContainer">
-                <div className="h-screen">
+                <div id="FlipUiDiv" className="h-screen">
                     <FlipWordsDemo/>
                 </div>
                 <Link to={"/portfolio_front_end"} className="text-white bg-white bg-opacity-10 border-[1.5px] font-['Inter'] font-medium rounded-full decoration-1 py-4 px-8 transition-all duration-300 hover:bg-white hover:text-black">Back to the main page</Link>
